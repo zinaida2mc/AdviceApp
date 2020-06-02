29eddfa6c719bf9b94887a5a0c96ae1f597d1983
Zi <Zinaida.d@2muchcoffee.com>
2020-06-03 01:12:17 +0300

    feat(components): update App with navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './Home';
 
+const Stack = createStackNavigator();
+
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'Home'} component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
